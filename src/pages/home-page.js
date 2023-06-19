dfd587a4ad37789fe54dd006764524d8dcb35a38
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-19 11:48:53 -0600

    Reimported homePageCards

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -3,7 +3,7 @@ import React from "react";
 // Components
 import HomePageBanner from "../components/homePageBanner/homePageBanner";
 import HomePageCaption from "../components/homePageCaption/homePageCaption";
-// import HomePageCards from '../components/homePageCards/homePageCards';
+import HomePageCards from '../components/homePageCards/homePageCards';
 import HomePageColumn from "../components/homePageColumn/homePageColumn";
 // import HomePageLinks from "../components/homePageLinks/homePageLinks";
 
@@ -12,7 +12,7 @@ export default function Home() {
         <div>
             <HomePageBanner />
             <HomePageCaption />
-            {/* <HomePageCards /> */}
+            <HomePageCards />
             <HomePageColumn />
             {/* <HomePageLinks /> */}
         </div>
