1f1ceb2d0039539d3f8b658c595cfe793c19deef
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 13:00:12 -0600

    Create blog-page.js

---
+++ src/pages/blog-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+// import BlogBanner from "../components/blogPage/blogBanner";
+// import BlogColumns from "../components/blogPage/blogColumns";
+
+export default function Blog() {
+    return (
+        <div>
+            {/* <BlogBanner /> */}
+            {/* <BlogColumns /> */}
+        </div>
+    );
+};
