071897ca48225916f22a427a3983eae7141270b0
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 13:08:46 -0600

    Update blog-page.js

--- src/pages/blog-page.js
+++ src/pages/blog-page.js
@@ -2,13 +2,13 @@ import React from "react";
 
 // Components
 import BlogBanner from "../components/blogPage/blogBanner";
-// import BlogColumns from "../components/blogPage/blogColumns";
+import BlogColumns from "../components/blogPage/blogColumns";
 
 export default function Blog() {
     return (
         <div>
             <BlogBanner />
-            {/* <BlogColumns /> */}
+            <BlogColumns />
         </div>
     );
 };
