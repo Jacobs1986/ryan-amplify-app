a850149d05a1fff6d8c205bc7d472249e08bcf86
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 11:56:41 -0600

    Create retreat-page.js

---
+++ src/pages/retreat-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+// import RetreatHeader from "../components/retreatPage/reatreatHeader";
+// import RetreatColumn from "../components/retreatPage/retreatColumn";
+
+export default function Retreat() {
+    return (
+        <div>
+            {/* <RetreatHeader /> */}
+            {/* <RetreatColumn /> */}
+        </div>
+    );
+};
