c537ec54628b93c5c867253c7fb39bbd695e1794
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 11:58:24 -0600

    Imported retreat page components

---
+++ src/components/retreatPage/reatreatHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// CSS File
+import "./retreat.css";
+
+export default function RetreatHeader() {
+    return (
+        <div>
+            <h1 className="centerText">
+                Nature Pods Retreat
+            </h1>
+        </div>
+    );
+};
