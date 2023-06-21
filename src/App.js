558f9b26781d665a54dd74a1e3c843cf498b6b64
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 11:57:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Footer from "./components/footer/footer";
 import Home from "./pages/home-page";
 import About from "./pages/about-page";
 import Media from "./pages/media-page";
+import Retreat from "./pages/retreat-page";
 
 function App() {
     return (
@@ -26,6 +27,7 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/media' element={<Media />} />
+                    <Route path='/retreat' element={<Retreat />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
