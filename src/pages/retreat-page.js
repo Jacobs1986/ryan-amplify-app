7d40e6a18bac9b5d206d0998c4420072a643efcb
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 11:59:08 -0600

    Update retreat-page.js

--- src/pages/retreat-page.js
+++ src/pages/retreat-page.js
@@ -2,13 +2,13 @@ import React from "react";
 
 // Components
 import RetreatHeader from "../components/retreatPage/reatreatHeader";
-// import RetreatColumn from "../components/retreatPage/retreatColumn";
+import RetreatColumn from "../components/retreatPage/retreatColumn";
 
 export default function Retreat() {
     return (
         <div>
             <RetreatHeader />
-            {/* <RetreatColumn /> */}
+            <RetreatColumn />
         </div>
     );
 };
