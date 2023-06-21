6eb9d159dc7850b4fcc83cb824e6e75e1eae8aaf
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-21 13:35:26 -0600

    Imported contactForm component

---
+++ src/components/contactForm/contactForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// CSS File
+import "./contactForm.css";
+
+export default function ContactForm() {
+    return (
+        <div>
+            <a href="mailto:mindfulnatureryan@gmail.com" className='messageLink'>
+                Click here to contact me
+            </a>
+        </div>
+    );
+};
